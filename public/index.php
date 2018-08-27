32f52a76724084c274cb262ecf62c23388c7d1e8
Nofrets Poai <opetstudio@gmail.com>
2018-08-27 22:06:51 +0700

    cors config

--- public/index.php
+++ public/index.php
@@ -7,6 +7,22 @@
 use Zend\Stdlib\ArrayUtils;
 use ZF\Apigility\Application;
 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+}
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
