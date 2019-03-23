8fbe0499a0b69ce6afef0d8cc8f948754fab95eb
Nofrets Poai <opetstudio@gmail.com>
2019-03-23 08:07:06 +0700

    set limit tb_gallery 1000

--- module/dashboard/config/module.config.php
+++ module/dashboard/config/module.config.php
@@ -488,7 +488,7 @@ return [
                 1 => 'POST',
             ],
             'collection_query_whitelist' => [],
-            'page_size' => '200',
+            'page_size' => '1000',
             'page_size_param' => null,
             'entity_class' => \dashboard\V1\Rest\TbGallery\TbGalleryEntity::class,
             'collection_class' => \dashboard\V1\Rest\TbGallery\TbGalleryCollection::class,
